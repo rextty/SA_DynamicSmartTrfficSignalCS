fa0272a4a7f36325dd486b65c83fbb5cee9f3d74
lanca000 <134751486+lanca000@users.noreply.github.com>
2023-06-01 20:59:07 +0800

    Update TrafficSignal.java

--- src/Model/TrafficSignal.java
+++ src/Model/TrafficSignal.java
@@ -1,9 +1,11 @@
 package Model;
 
 // TODO: TrafficSignal
+// 如果有懂java的人看到ㄉ話，歡迎幫忙修改，我主要放我的思路供參考
 public class TrafficSignal {
     //private int countTime;
-    private int signalState;
+    private int currentState;
+    private int direction;  //資料型態參照類別圖
     
     //想將上方的countTime拆分成如下之各燈號時長屬性，原因：
     //1.判斷後壅塞狀況後，做的調整是eg.NS更塞 會增加NS向綠燈時間 && 減少WE向綠燈時間，表示紅燈也該相應的被調整到
@@ -12,9 +14,78 @@ public class TrafficSignal {
     private int yellowLightDuration;
     private int redLightDuration;
     
-    public TrafficSignal() {}
+    // 傳入的參數direction->以建立不同方向的紅綠燈物件
+    public TrafficLight(char direction, int greenDuration, int yellowDuration, int redDuration) {
+        this.direction = direction;
+        this.greenDuration = greenDuration;
+        this.yellowDuration = yellowDuration;
+        this.redDuration = redDuration;
+        this.currentState = "red"; // 初始狀態為紅燈
+    }
+    
+    // 在我們的應用中紅綠燈一開始便是啟動狀態
+	// start()和 TrafficLight物件的建立好像沒出現在我們的流程之中，這部分該怎麼處理
+    public void start() {
+        // 啟動紅綠燈，初始狀態為綠燈
+        currentState = "green";
+        performSignal();
 
-    public int getSignalState() {return 0;}
+        // 開始進行狀態切換
+        scheduleNextTransition();
+    }
+	
+    // 根據當前燈號狀態切換到下一個狀態
+    public void transitionToNextState() {
+        switch (currentState) {
+            case "green":
+                currentState = "yellow";
+                break;
+            case "yellow":
+                currentState = "red";
+                break;
+            case "red":
+                currentState = "green";
+                break;
+        }
+
+        performSignal();
+        scheduleNextTransition();
+    }
+
+    // 根據當前燈號狀態選擇相應的持續時間，然後通過 Thread.sleep(duration * 1000) 將程式暫停指定的時間，從而控制狀態切換的時間間隔
+    public void scheduleNextTransition() {
+        int duration;
+        switch (currentState) {
+            case "green":
+                duration = greenDuration;
+                break;
+            case "yellow":
+                duration = yellowDuration;
+                break;
+            case "red":
+                duration = redDuration;
+                break;
+            default:
+                duration = redDuration;
+        }
+
+        try {
+            Thread.sleep(duration * 1000); // 單位轉換為毫秒
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        transitionToNextState();
+    }
+
+    public int getCurrentState() {return currentState;}
+    
+    // 車流跟緊急車輛的兩種功能好像都不會動到黃燈？
+    public void setDurations(int green, int red) {
+        this.greenDuration = green;
+        //this.yellowDuration = yellow; 
+        this.redDuration = red;
+    }
     
     //採取預設值（不確定大家共識的各燈號時間長度，暫參考張庭碩的word檔）
     public void adoptDefault() {
@@ -23,6 +94,9 @@ public class TrafficSignal {
         redLightDuration = 30;
     }
     
-    //還在思考燈號拆開後的變換和循序圖中想要做到的NS/WE時間調整
-    public void changeSignal(int roadID, int time, int signalState) {}
+    // 循序圖中最後顯示給vehicle的帕，不確定這部分是不是需要跟我們模擬的道路畫面相連接
+    public void performSignal(){};
+    
+    //原本的設置燈號函數
+    //public void changeSignal(int roadID, int time, int signalState) {}
 }
