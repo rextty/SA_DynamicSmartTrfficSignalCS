9c28d3ef87a40336d18e466a5f411cfc22a595b7
lanca000 <134751486+lanca000@users.noreply.github.com>
2023-06-01 22:01:31 +0800

    Update RoadScoreCalculator.java

--- src/Model/RoadScoreCalculator.java
+++ src/Model/RoadScoreCalculator.java
@@ -4,13 +4,29 @@ import DAO.Road;
 
 import java.util.ArrayList;
 
+// 有懂java的人看到，歡迎修改，我的內容僅提供自己的思路以供參考
 public class RoadScoreCalculator {
 
-    private int roadScore;
+    // 照循序圖，計算道路分數時有區分成NS與WE兩種，故拆開
+    // 不太確定若一開始便設為零可不可行
+    private int WEroadScore = 0;
+	private int NSroadScore = 0;
 
     public RoadScoreCalculator() {}
 
-    int getRoadScore() {return 0;}
+    public int getWERoadScore() {return WEroadScore;}
+	public int getNSRoadScore() {return NSroadScore;}
 
-    void calRoadScore(ArrayList<Road> roads) {}
+    // 先判斷road是哪個方向，再去做NS或WE的加總
+    public void calRoadScore(ArrayList<Road> roads) {
+        for (Road road: roads) {
+			// direction是int，具體數值是?
+			if(road.direction == 'N' || road.direction == 'S'){
+				NSroadScore += road.getScore();
+			}
+			if(road.direction == 'W' || road.direction == 'E'){
+				WEroadScore += road.getScore();
+			}
+        }
+    }
 }
