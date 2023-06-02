212dbe35b7204fdfbaf92efb90874a3de588d29c
lanca000 <134751486+lanca000@users.noreply.github.com>
2023-06-02 15:19:52 +0800

    Update TrafficSignal.java

--- src/Model/TrafficSignal.java
+++ src/Model/TrafficSignal.java
@@ -10,9 +10,9 @@ public class TrafficSignal {
     //想將上方的countTime拆分成如下之各燈號時長屬性，原因：
     //1.判斷後壅塞狀況後，做的調整是eg.NS更塞 會增加NS向綠燈時間 && 減少WE向綠燈時間，表示紅燈也該相應的被調整到
     //2.緊急車輛出現時，路口中各個燈號的時長處理是更複雜的，拆開會比較好操作
-    private int greenLightDuration;
-    private int yellowLightDuration;
-    private int redLightDuration;
+    private int greenDuration;
+    private int yellowDuration;
+    private int redDuration;
     
     /* 若要考慮到各路口燈號時間長度不一定，則需要額外設置屬性儲存預設值
     private int defaultGreen;
@@ -104,9 +104,9 @@ public class TrafficSignal {
     //採取預設值（不確定大家共識的各燈號時間長度，暫參考張庭碩的word檔）
     //不考慮各路口的燈號時間不一，直接將我們定的數字寫進去
     public void adoptDefault() {
-        greenLightDuration = 30;
-        yellowLightDuration = 5;
-        redLightDuration = 30;
+        greenDuration = 30;
+        yellowDuration = 5;
+        redDuration = 30;
 	    
 	/*若要考慮到各路口燈號時間長度不一定，則是透過預設值屬性去賦值
         greenDuration = this.defaultGreen;
