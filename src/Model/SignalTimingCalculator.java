03d5545ed960e8cbce31c99f470688669d72d725
lanca000 <134751486+lanca000@users.noreply.github.com>
2023-06-02 14:24:48 +0800

    Update SignalTimingCalculator.java

--- src/Model/SignalTimingCalculator.java
+++ src/Model/SignalTimingCalculator.java
@@ -2,10 +2,17 @@ package Model;
 
 public class SignalTimingCalculator {
 
-    private int signalTime;
+    // private int signalTime;
+    // public int getSignalTime() {return 0;}
+    
+    // 照循序圖，調整過後的秒數（綠燈）我們是分成NS和WE兩個
+    private int NSsignalTime;
+    private int WEsignalTime;
 
-    public int getSignalTime() {return 0;}
-
+    public int getNSsignalTime() {return NSsignalTime;}
+	public int getWEsignalTime() {return WEsignalTime;}
+    
+    // 要怎麼做到基於預設值去進行秒數的增減調整
     public void calSignalTime(int NS, int WE) {}
 
 }
